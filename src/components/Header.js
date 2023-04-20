eb94a727625ec3112dfff490f7f60142cdbeb61b
Topher Hook <topherhook@gmail.com>
2023-04-20 13:58:29 -0700

    HeartsList

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => <div className='App-header'>I ❤️ React</div>
+
+// const Header = () => {
+//     return (
+//         <div className='App-header'>I ❤️ React</div>
+//     );
+// }
+
+export default Header;
